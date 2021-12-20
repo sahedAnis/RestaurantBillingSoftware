afa11680cd08d0d90cf2c214c5dc00ac7da5ab41
sahed <anis.sahed.etu@univ-lille.fr>
2021-12-20 20:42:32 +0100

    Update Checking invoices + Select invoice by name

--- app.c
+++ app.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
 #define SIZE 1<<10
 
 struct item{
@@ -12,6 +20,7 @@ struct item{
 struct orders{
     char name[100];
     char date[50];
+    int numberOfItems;
     struct item items[SIZE];
 };
 
@@ -57,7 +66,9 @@ int invoiceHeader(const char* date, const char* name){
     printf("\t\tInvoice to : Mr. %s\n", name);
 
     printf("\t\t---------------------------------------------------------\n");
-    printf("\t\tItems\t\t\tQty\t\t\t    Total\n");
+    printf("\t\tItems");
+    printf("\t\t\tQty");
+    printf("\t\t\t    Total\n");
     printf("\t\t---------------------------------------------------------\n");
 
     return 0;
@@ -70,70 +81,132 @@ char invoiceBody(int nbitems, struct orders* ord){
     printf("\n");
     for(int i = 0; i < nbitems; i++){
         total = total + ord->items[i].qty * ord->items[i].uprice;
-        printf("\t\t%s\t\t\t%d\t\t\t   %2.f\n", ord->items[i].name, ord->items[i].qty, ord->items[i].qty * ord->items[i].uprice);
+        printf("\t\t%s", ord->items[i].name);
+        printf("\t\t\t%d", ord->items[i].qty);
+        printf("\t\t\t    %2.f\n", ord->items[i].qty * ord->items[i].uprice);
     }
     printf("\n");
     printf("\t\t---------------------------------------------------------\n");
-    printf("\t\tSub total\t\t\t\t\t   %2.f\n", total);
-    printf("\t\tDiscount @10%%\t\t\t\t\t   %.2f\n", total*0.1);
+    printf("\t\tSub total\t\t\t\t\t    %2.f\n", total);
+    printf("\t\tDiscount @10%%\t\t\t\t\t    %.2f\n", total*0.1);
 
     printf("\t\t\t\t\t\t\t\t---------\n");
-    printf("\t\tAmoout before tax\t\t\t\t   %.2f\n", total-(total*0.1));
-    printf("\t\tTVA @20%%\t\t\t\t\t   %.2f\n", (total-(total*0.1))*0.2);
+    printf("\t\tAmount before tax\t\t\t\t    %.2f\n", total-(total*0.1));
+    printf("\t\tTVA @20%%\t\t\t\t\t    %.2f\n", (total-(total*0.1))*0.2);
     printf("\t\t---------------------------------------------------------\n");
-    printf("\t\tTotal after tax\t\t\t\t\t   %.2f\n", (total-(total*0.1))+((total-(total*0.1))*0.2));
+    printf("\t\tTotal after tax\t\t\t\t\t    %.2f\n", (total-(total*0.1))+((total-(total*0.1))*0.2));
     printf("\t\t---------------------------------------------------------\n");
-    printf("\t\tDo you want to save the invoice? (y/n)");
-    scanf("%s", &save);
-    printf("\n");
-    return save;
+    return 0;
 }
 
 int main(){
     int choice;
     struct orders order1;
-    int items = 0;
+    struct orders savedorders;
+    char name[50];
+    FILE* fd = 0;
+    size_t written;
     char save;
 
     printf("\n\t\t\t\t\t\t========== WELCOME ===========\n\n");
     choice = selectOperation();
 
-    if (choice == 1){
-        printf("\t\tPlease enter your customer's name : ");
-        fgets(order1.name, 100, stdin);
+    printf("\t\tPlease enter your customer's name : ");
+    fgets(order1.name, 100, stdin);
         
-        order1.name[strlen(order1.name)-1] = 0;
+    order1.name[strlen(order1.name)-1] = 0;
+
+    while (choice == 1){
+        system("clear");
         
         printf("\n\t\tWelcome %s!\n", order1.name);
         
         strcpy(order1.date, __DATE__);
 
         printf("\n\t\tPlease enter the number of items : ");
-        scanf("%d", &items);
+        scanf("%d", &order1.numberOfItems);
         
-        for(int i = 0; i < items; i++){
+        for(int i = 0; i < order1.numberOfItems; i++){
             fgetc(stdin);
-            struct item itm;
             printf("\n\t\tPlease enter the item %d : ", i+1);
-            fgets(itm.name, 100, stdin);
-            itm.name[strlen(itm.name)-1] = 0;
+            fgets(order1.items[i].name, 100, stdin);
+            order1.items[i].name[strlen(order1.items[i].name)-1] = 0;
 
             printf("\t\tPlease enter the unit price : ");
-            scanf("%f", &itm.uprice);
+            scanf("%f", &order1.items[i].uprice);
 
             printf("\t\tPlease enter the quantity : ");
-            scanf("%d", &itm.qty);
-
-            order1.items[i] = itm;
+            scanf("%d", &order1.items[i].qty);
         }
         printf("\n");
 
         invoiceHeader(order1.date, order1.name);
-        save = invoiceBody(items, &order1);
+        invoiceBody(order1.numberOfItems, &order1);
+        printf("\n\t\tDo you want to save the invoice? (y/n) ");
+        scanf("%s", &save);
+        fgetc(stdin);
 
         if (save == 'y'){
-            printf("\t\tAzulaaa");
+            fd = fopen("RestaurantBills.dat", "a+");
+            written = fwrite(&order1, sizeof(struct orders), 1, fd);
+            if(written != 0){
+                printf("\t\tSuccesfully saved!, written : %d\n", fd);
+                printf("\n\t\tWant to create another invoice? Please put 1.\n");
+                printf("\t\tSee your previous invoices ? Please put 2.\n");
+                printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+                printf("\t\tExit ? Please put 4.\n");
+                printf("\n\t\tYour choice : ");
+                scanf("%d", &choice);
+            }
+            else{
+                printf("Error saving the invoice..\n");
+            }
+            fclose(fd);
         }
-    
     }
+
+        while (choice == 2){
+            system("clear");
+            fd = fopen("RestaurantBills.dat", "r");
+            printf("\t\t****Your previous invoices****\n");
+            while(fread(&savedorders, sizeof(struct orders), 1, fd)){
+                invoiceHeader(&savedorders.date, &savedorders.name);
+                invoiceBody(savedorders.numberOfItems, &savedorders);
+            }
+            printf("\n\t\tWant to create another invoice? Please put 1.\n");
+            printf("\t\tSee your previous invoices ? Please put 2.\n");
+            printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+            printf("\t\tExit ? Please put 4.\n");
+            printf("\n\t\tYour choice : ");
+            scanf("%d", &choice);
+            close(fd);
+        }
+
+        int cmp = -1;
+
+        while(choice == 3){
+            int found = 0;
+            system("clear");
+            fd = open("RestaurantBills.dat", O_RDONLY);
+            printf("\n\t\tPlease put a name : ");
+            scanf("%s", &name);
+            while(read(fd, &savedorders, sizeof(struct orders)) && found != 1){
+                cmp = strcmp(name, savedorders.name);
+                if(cmp == 0){
+                    invoiceHeader(&savedorders.date, &savedorders.name);
+                    invoiceBody(savedorders.numberOfItems, &savedorders);
+                    found = 1;
+                }
+            }
+            if (found == 0){
+                printf("\t\tInvoice for the name %s does not exists.\n", name);
+            }
+            printf("\n\t\tWant to create another invoice? Please put 1.\n");
+            printf("\t\tSee your previous invoices ? Please put 2.\n");
+            printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+            printf("\t\tExit ? Please put 4.\n");
+            printf("\n\t\tYour choice : ");
+            scanf("%d", &choice);
+            close(fd);
+        }
 }
