3803e9cfaa0e169e8f1bcb59017dcea44ecc1ed6
sahed <anis.sahed.etu@univ-lille.fr>
2021-12-21 14:41:53 +0100

    FINAL V1

--- app.c
+++ app.c
@@ -99,114 +99,339 @@ char invoiceBody(int nbitems, struct orders* ord){
     return 0;
 }
 
+int choiceOne(struct orders ord, struct orders ord2){
+    system("clear");
+    int choice = 0;
+    size_t written;
+    FILE* fd = 0;
+    char save;
+
+        
+    strcpy(ord.date, __DATE__);
+
+    printf("\n\t\tPlease enter the number of items : ");
+    scanf("%d", &ord.numberOfItems);
+        
+    for(int i = 0; i < ord.numberOfItems; i++){
+        fgetc(stdin);
+        printf("\n\t\tPlease enter the item %d : ", i+1);
+        fgets(ord.items[i].name, 100, stdin);
+        ord.items[i].name[strlen(ord.items[i].name)-1] = 0;
+
+        printf("\t\tPlease enter the unit price : ");
+        scanf("%f", &ord.items[i].uprice);
+
+        printf("\t\tPlease enter the quantity : ");
+        scanf("%d", &ord.items[i].qty);
+    }
+    printf("\n");
+
+    invoiceHeader(ord.date, ord.name);
+    invoiceBody(ord.numberOfItems, &ord);
+    printf("\n\t\tDo you want to save the invoice? (y/n) ");
+    scanf("%s", &save);
+    fgetc(stdin);
+
+    if (save == 'y'){
+        fd = fopen("RestaurantBills.dat", "a+");
+        written = fwrite(&ord, sizeof(struct orders), 1, fd);
+        if(written != 0){
+            printf("\t\tSuccesfully saved!, written : %d\n", fd);
+            printf("\n\t\tWant to create another invoice? Please put 1.\n");
+            printf("\t\tSee your previous invoices ? Please put 2.\n");
+            printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+            printf("\t\tExit ? Please put 4.\n");
+            printf("\n\t\tYour choice : ");
+            scanf("%d", &choice);
+            if (choice == 1){
+                choice = choiceOne(ord, ord2);
+                while(choice == 1){
+                    choice = choiceOne(ord, ord2);
+                }
+                while(choice == 2){
+                    choice = choiceTwo(ord2, ord);
+                }
+                while(choice == 3){
+                    choice = choiceThree(ord2, ord);
+                }
+            }
+
+            if (choice == 2){
+                choice = choiceTwo(ord2, ord);
+                while(choice == 1){
+                    choice = choiceOne(ord, ord2);
+                }
+                while(choice == 2){
+                    choice = choiceTwo(ord2, ord);
+                }
+                while(choice == 3){
+                    choice = choiceThree(ord2, ord);
+                }
+            }
+
+            if (choice == 3){
+                choice = choiceThree(ord2, ord);
+                while(choice == 1){
+                    choice = choiceOne(ord, ord2);
+                }
+                while(choice == 2){
+                    choice = choiceTwo(ord2, ord);
+                }
+                while(choice == 3){
+                    choice = choiceThree(ord2, ord);
+                }
+
+            }
+
+            if(choice == 4){
+                printf("\n\t\tProgram exit...\n");
+                sleep(1);
+                printf("\n\t\tThank you! See you soon!\n");
+                exit(0);
+            }
+        }
+    }
+    else if (save == 'n'){
+        printf("\t\tYou have chosen not to save the invoice.\n");
+        printf("\n\t\tWant to create another invoice? Please put 1.\n");
+        printf("\t\tSee your previous invoices ? Please put 2.\n");
+        printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+        printf("\t\tExit ? Please put 4.\n");
+        printf("\n\t\tYour choice : ");
+        scanf("%d", &choice);
+        if (choice == 1){
+            choice = choiceOne(ord, ord2);
+            while(choice == 1){
+                choice = choiceOne(ord, ord2);
+            }
+            while(choice == 2){
+                choice = choiceTwo(ord2, ord);
+            }
+            while(choice == 3){
+                choice = choiceThree(ord2, ord);
+            }
+        }
+
+        if (choice == 2){
+            choice = choiceTwo(ord2, ord);
+            while(choice == 1){
+                choice = choiceOne(ord, ord2);
+            }
+            while(choice == 2){
+                choice = choiceTwo(ord2, ord);
+            }
+            while(choice == 3){
+                choice = choiceThree(ord2, ord);
+            }
+        }
+
+        if (choice == 3){
+            choice = choiceThree(ord2, ord);
+            while(choice == 1){
+                choice = choiceOne(ord, ord2);
+            }
+            while(choice == 2){
+                choice = choiceTwo(ord2, ord);
+            }
+            while(choice == 3){
+                choice = choiceThree(ord2, ord);
+            }
+        }
+
+        if(choice == 4){
+            printf("\n\t\tProgram exit...\n");
+            sleep(1);
+            printf("\n\t\tThank you! See you soon!\n");
+            exit(0);
+        }
+        fclose(fd);
+        
+    }
+    else{
+        printf("\n\t\tERROR SAVING THE INVOICE...\n");
+        exit(0);
+    }
+    return choice;
+}
+    
+
+int choiceTwo(struct orders ord, struct orders ord2){
+    system("clear");
+    int choice = 0;
+    FILE* fd;
+    fd = fopen("RestaurantBills.dat", "r");
+    printf("\t\t****Your previous invoices****\n");
+    while(fread(&ord, sizeof(struct orders), 1, fd)){
+        invoiceHeader(&ord.date, &ord.name);
+        invoiceBody(ord.numberOfItems, &ord);
+    }
+    printf("\n\t\tWant to create another invoice? Please put 1.\n");
+    printf("\t\tSee your previous invoices ? Please put 2.\n");
+    printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+    printf("\t\tExit ? Please put 4.\n");
+    printf("\n\t\tYour choice : ");
+    scanf("%d", &choice);
+    close(fd);
+    if (choice == 1){
+        choice = choiceOne(ord, ord2);
+        while(choice == 1){
+            choice = choiceOne(ord, ord2);
+        }
+        while(choice == 2){
+            choice = choiceTwo(ord2, ord);
+        }
+        while(choice == 3){
+            choice = choiceThree(ord2, ord);
+        }
+    }
+
+    if (choice == 2){
+        choice = choiceTwo(ord2, ord);
+        while(choice == 1){
+            choice = choiceOne(ord, ord2);
+        }
+        while(choice == 2){
+            choice = choiceTwo(ord2, ord);
+        }
+        while(choice == 3){
+            choice = choiceThree(ord2, ord);
+        }
+    }
+
+    if (choice == 3){
+        choice = choiceThree(ord2, ord);
+        while(choice == 1){
+            choice = choiceOne(ord, ord2);
+        }
+        while(choice == 2){
+            choice = choiceTwo(ord2, ord);
+        }
+        while(choice == 3){
+            choice = choiceThree(ord2, ord);
+        }
+    }
+
+    if(choice == 4){
+        printf("\n\t\tProgram exit...\n");
+        sleep(1);
+        printf("\n\t\tThank you! See you soon!\n");
+        exit(0);
+    }
+
+    return choice;
+}
+
+int choiceThree(struct orders ord, struct orders ord2){
+    FILE* fd;
+    int choice;    
+    int found = 0;
+    char name[50];
+    system("clear");
+    fd = open("RestaurantBills.dat", O_RDONLY);
+    printf("\n\t\tPlease put a name : ");
+    scanf("%s", &name);
+    int cmp = -1;
+    while(read(fd, &ord, sizeof(struct orders)) && found != 1){
+        cmp = strcmp(name, ord.name);
+        if(cmp == 0){
+            invoiceHeader(&ord.date, &ord.name);
+            invoiceBody(ord.numberOfItems, &ord);
+            found = 1;
+        }
+    }
+    if (found == 0){
+        printf("\t\tInvoice for the name %s does not exists.\n", name);
+    }
+    printf("\n\t\tWant to create another invoice? Please put 1.\n");
+    printf("\t\tSee your previous invoices ? Please put 2.\n");
+    printf("\t\tSearch for an invoice by name ? Please put 3.\n");
+    printf("\t\tExit ? Please put 4.\n");
+    printf("\n\t\tYour choice : ");
+    scanf("%d", &choice);
+    if (choice == 1){
+        choice = choiceOne(ord, ord2);
+        while(choice == 1){
+            choice = choiceOne(ord, ord2);
+        }
+        while(choice == 2){
+            choice = choiceTwo(ord2, ord);
+        }
+        while(choice == 3){
+            choice = choiceThree(ord2, ord);
+        }
+    }
+
+    if (choice == 2){
+        choice = choiceTwo(ord2, ord);
+        while(choice == 1){
+            choice = choiceOne(ord, ord2);
+        }
+        while(choice == 2){
+            choice = choiceTwo(ord2, ord);
+        }
+        while(choice == 3){
+            choice = choiceThree(ord2, ord);
+        }
+    }
+
+    if (choice == 3){
+        choice = choiceThree(ord2, ord);
+        while(choice == 1){
+            choice = choiceOne(ord, ord2);
+        }
+        while(choice == 2){
+            choice = choiceTwo(ord2, ord);
+        }
+        while(choice == 3){
+            choice = choiceThree(ord2, ord);
+        }
+    }
+
+    if(choice == 4){
+        printf("\n\t\tProgram exit...\n");
+        sleep(1);
+        printf("\n\t\tThank you! See you soon!\n");
+        exit(0);
+        }
+    close(fd);
+    return choice;
+}
+
+
 int main(){
     int choice;
     struct orders order1;
     struct orders savedorders;
-    char name[50];
-    FILE* fd = 0;
-    size_t written;
-    char save;
+    char name[50]; 
 
     printf("\n\t\t\t\t\t\t========== WELCOME ===========\n\n");
-    choice = selectOperation();
 
     printf("\t\tPlease enter your customer's name : ");
     fgets(order1.name, 100, stdin);
+    
         
     order1.name[strlen(order1.name)-1] = 0;
+    printf("\n\t\tWelcome %s!\n", order1.name);
+    sleep(1);
 
-    while (choice == 1){
-        system("clear");
-        
-        printf("\n\t\tWelcome %s!\n", order1.name);
-        
-        strcpy(order1.date, __DATE__);
+    choice = selectOperation();
 
-        printf("\n\t\tPlease enter the number of items : ");
-        scanf("%d", &order1.numberOfItems);
-        
-        for(int i = 0; i < order1.numberOfItems; i++){
-            fgetc(stdin);
-            printf("\n\t\tPlease enter the item %d : ", i+1);
-            fgets(order1.items[i].name, 100, stdin);
-            order1.items[i].name[strlen(order1.items[i].name)-1] = 0;
-
-            printf("\t\tPlease enter the unit price : ");
-            scanf("%f", &order1.items[i].uprice);
-
-            printf("\t\tPlease enter the quantity : ");
-            scanf("%d", &order1.items[i].qty);
-        }
-        printf("\n");
-
-        invoiceHeader(order1.date, order1.name);
-        invoiceBody(order1.numberOfItems, &order1);
-        printf("\n\t\tDo you want to save the invoice? (y/n) ");
-        scanf("%s", &save);
-        fgetc(stdin);
-
-        if (save == 'y'){
-            fd = fopen("RestaurantBills.dat", "a+");
-            written = fwrite(&order1, sizeof(struct orders), 1, fd);
-            if(written != 0){
-                printf("\t\tSuccesfully saved!, written : %d\n", fd);
-                printf("\n\t\tWant to create another invoice? Please put 1.\n");
-                printf("\t\tSee your previous invoices ? Please put 2.\n");
-                printf("\t\tSearch for an invoice by name ? Please put 3.\n");
-                printf("\t\tExit ? Please put 4.\n");
-                printf("\n\t\tYour choice : ");
-                scanf("%d", &choice);
-            }
-            else{
-                printf("Error saving the invoice..\n");
-            }
-            fclose(fd);
-        }
+    if (choice == 1){
+        choiceOne(order1, savedorders);
     }
 
-        while (choice == 2){
-            system("clear");
-            fd = fopen("RestaurantBills.dat", "r");
-            printf("\t\t****Your previous invoices****\n");
-            while(fread(&savedorders, sizeof(struct orders), 1, fd)){
-                invoiceHeader(&savedorders.date, &savedorders.name);
-                invoiceBody(savedorders.numberOfItems, &savedorders);
-            }
-            printf("\n\t\tWant to create another invoice? Please put 1.\n");
-            printf("\t\tSee your previous invoices ? Please put 2.\n");
-            printf("\t\tSearch for an invoice by name ? Please put 3.\n");
-            printf("\t\tExit ? Please put 4.\n");
-            printf("\n\t\tYour choice : ");
-            scanf("%d", &choice);
-            close(fd);
-        }
+    if (choice == 2){
+        choiceTwo(savedorders, order1);
+    }
 
-        int cmp = -1;
+    if (choice == 3){
+        choiceThree(savedorders, order1);
+    }
 
-        while(choice == 3){
-            int found = 0;
-            system("clear");
-            fd = open("RestaurantBills.dat", O_RDONLY);
-            printf("\n\t\tPlease put a name : ");
-            scanf("%s", &name);
-            while(read(fd, &savedorders, sizeof(struct orders)) && found != 1){
-                cmp = strcmp(name, savedorders.name);
-                if(cmp == 0){
-                    invoiceHeader(&savedorders.date, &savedorders.name);
-                    invoiceBody(savedorders.numberOfItems, &savedorders);
-                    found = 1;
-                }
-            }
-            if (found == 0){
-                printf("\t\tInvoice for the name %s does not exists.\n", name);
-            }
-            printf("\n\t\tWant to create another invoice? Please put 1.\n");
-            printf("\t\tSee your previous invoices ? Please put 2.\n");
-            printf("\t\tSearch for an invoice by name ? Please put 3.\n");
-            printf("\t\tExit ? Please put 4.\n");
-            printf("\n\t\tYour choice : ");
-            scanf("%d", &choice);
-            close(fd);
-        }
+    if(choice == 4){
+        printf("\n\t\tProgram exit...\n");
+        sleep(1);
+        printf("\n\t\tThank you! See you soon!\n");
+        exit(0);
+    }
 }
